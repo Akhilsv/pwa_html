70db3b0f2b770b6b1da3c37df3dbadb1313bb71e
Alan-Biju <alanbiju238@gmail.com>
2022-07-12 00:09:44 +0530

    pwa

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
 	navigator.serviceWorker
-		.register('serviceWorker.js')
+		.register('./serviceWorker.js')
 		.then((registration) => {
 			console.log('SW register');
 			console.log(registration);
