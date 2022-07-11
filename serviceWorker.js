8b5b876f69491f1de5505ee114f0098cf486a3c9
Alan-Biju <alanbiju238@gmail.com>
2022-07-12 00:02:07 +0530

    pwa

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,43 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("static").then(cache => {
-            return cache.addAll(["./", './style', './images/logo.png','./assets']);
-        })
-    )
-    // console.log("Install!")
-})
-self.addEventListener("fetch", e => {
-    // console.log(e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(res => {
-            return res || fetch(e.request);
-        })
-    )
-})
+const CACHE_NAME = 'version-1';
+const urlsToCache = ['./', './style', './images/logo.png', './assets'];
+
+const self = this;
+
+// Install SW
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => {
+			console.log('Opened cache');
+
+			return cache.addAll(urlsToCache);
+		}),
+	);
+});
+
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+	event.respondWith(
+		caches.match(event.request).then(() => {
+			return fetch(event.request).catch(() => caches.match('offline.html'));
+		}),
+	);
+});
+
+// Activate the SW
+self.addEventListener('activate', (event) => {
+	const cacheWhitelist = [];
+	cacheWhitelist.push(CACHE_NAME);
+
+	event.waitUntil(
+		caches.keys().then((cacheNames) =>
+			Promise.all(
+				// eslint-disable-next-line array-callback-return
+				cacheNames.map((cacheName) => {
+					if (!cacheWhitelist.includes(cacheName)) {
+						return caches.delete(cacheName);
+					}
+				}),
+			),
+		),
+	);
+});
